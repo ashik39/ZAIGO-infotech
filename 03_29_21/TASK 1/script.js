9a4ef238d5a6a5d13988b31807306d6f660fc193
unknown <asanashik7@gmail.com>
2021-03-29 12:19:27 +0530

    HTML content created for TASK1

---
+++ 03_29_21/TASK 1/script.js
@@ -0,0 +1,13 @@
+const firstname = document.getElementById('fname');
+const lastname = document.getElementById('lname');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const country = document.getElementById('country');
+const state = document.getElementById('state');
+
+const page1= document.getElementById('pg1');
+const page2= document.getElementById('pg2');
+const page2= document.getElementById('pg3');
+const page2= document.getElementById('pg4');
+
+
